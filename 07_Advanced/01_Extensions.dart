4c029cbef6e872b7bbe72d09996712a8c0c1db0e
刘德龙 <793854601@qq.com>
2023-03-05 01:32:17 +0800

    Dart进阶

--- 07_Advanced/01_Extensions.dart
+++ 07_Advanced/01_Extensions.dart
@@ -2,12 +2,17 @@ void main(List<String> args) {
   var string = '123';
   print(string.clazzType);
   print(string.parseInt());
+
+  10.repeat((index) {
+    print('index = $index');
+  });
+
+  print(10.generate((index) => 'string: $index'));
 }
 
 //  使用extension，为现有的类添加内容
 //  可以添加getter、setter、方法
 extension StringExt on String {
-
   //  扩展getter
   String get clazzType => runtimeType.toString();
 
@@ -18,6 +23,22 @@ extension StringExt on String {
       result = int.parse(this);
     } catch (e) {
       print(e);
+    }
+    return result;
+  }
+}
+
+extension IntExt on int {
+  void repeat(void Function(int index) block) {
+    for (var i = 0; i < this; i++) {
+      block(i);
+    }
+  }
+
+  List<E> generate<E>(E Function(int index) block) {
+    final result = <E>[];
+    for (var i = 0; i < this; i++) {
+      result.add(block(i));
     }
     return result;
   }
