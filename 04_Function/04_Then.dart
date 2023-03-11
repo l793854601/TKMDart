7ea6792879cce9c762a4d0f3360ee005b7572c40
刘德龙 <793854601@qq.com>
2023-03-11 13:11:59 +0800

    修改Then示例

--- 04_Function/04_Then.dart
+++ 04_Function/04_Then.dart
@@ -1,23 +1,12 @@
-import 'package:http/http.dart' as http;
-
-//  使用then
-// void main(List<String> args) {
-//   getIPAddress()
-//       .then((value) => print(value.body))
-//       .catchError((error) => print(error));
-// }
-
-//  使用async、await
-void main(List<String> args) async {
-  try {
-    http.Response response = await getIPAddress();
-    print(response.body);
-  } catch (e) {
-    print(e);
-  }
-}
-
-Future<http.Response> getIPAddress() {
-  final url = 'https://httpbin.org/ip';
-  return http.get(Uri.parse(url));
+void main(List<String> args) {
+  Future.value('Step1').then((value) {
+    print('$value completed');
+    return 'Step2';
+  }).then((value) {
+    print('$value completed');
+    return 'Step3';
+  }).then((value) {
+    print('$value completed');
+    print('All completed');
+  });
 }
